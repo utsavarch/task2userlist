f8be7b880fcd97dd2c3cea2127822ab401e1e853
Utsav Acharya Sharma <71218506+utsavarch@users.noreply.github.com>
2024-09-16 14:31:52 +0545

    Update index.js

--- src/apiLayer/index.js
+++ src/apiLayer/index.js
@@ -1,18 +1,19 @@
-import axios from "axios";
-
 export const baseUrl = "https://jsonplaceholder.typicode.com/";
-
-export const apiCallInstance = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
 
 export const fetchUsers = async () => {
   try {
-    const response = await apiCallInstance.get("users");
-    return response.data;
+    const response = await fetch(`${baseUrl}users`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch users");
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.log(error);
     throw error;
